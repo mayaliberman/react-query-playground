c4e22837149b532ea30bcee75427d4552f28c009
Maya Liberman <mayaliberman1@gmail.com>
2021-12-01 20:07:29 -0500

    adding gh configuration

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Planet from './Planet';
-const fetchPlanets = async (key, pageNum) => {
+const fetchPlanets = async (pageNum = 1) => {
 	const res = await fetch(`https://swapi.dev/api/planets/?page=${pageNum}`);
 	return res.json();
 };
@@ -9,25 +9,44 @@ const fetchPlanets = async (key, pageNum) => {
 const Planets = () => {
 	const [page, setPage] = useState(1);
 
-	const { data, status } = useQuery(['planets', page], ({ queryKey }) =>
-		fetchPlanets(queryKey[0], page)
+	const { data, isLoading, isError, isPreviousData, isFetching } = useQuery(
+		['planets', page],
+		() => fetchPlanets(page),
+		{ keepPreviousData: true }
 	);
 
 	return (
 		<div>
 			<h2>Planets</h2>
-			<button onClick={() => setPage(1)}>Page No.1</button>
-			<button onClick={() => setPage(2)}>Page No.2</button>
-			<button onClick={() => setPage(3)}>Page No.3</button>
-			{status === 'loading' && <div>Loading data </div>}
-			{status === 'success' && (
+			<h3>Current Page: {page}</h3>
+			<button
+				onClick={() => setPage((old) => Math.max(old - 1, 0))}
+				disabled={page === 1}
+			>
+				Previous Page
+			</button>
+			<button
+				onClick={() => {
+					if (!isPreviousData && data.next) {
+						setPage((old) => old + 1);
+					}
+				}}
+				disabled={isPreviousData || !data?.next}
+			>
+				Next Page
+			</button>
+			{isFetching ? <span> Loading...</span> : null}{' '}
+			{isLoading ? (
+				<div>Loading...</div>
+			) : isError ? (
+				<div>Error</div>
+			) : (
 				<div>
 					{data.results.map((planet) => (
 						<Planet planet={planet} key={planet.name} />
 					))}
 				</div>
 			)}
-			{status === 'error' && <div>Error fetching data </div>}
 		</div>
 	);
 };
